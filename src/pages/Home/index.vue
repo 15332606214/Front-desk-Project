6dd139d6f78f6c6003694419da23709396e46203
gao <gao@qq.com>
2022-11-28 23:26:55 +0800

    first

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-    Home
+    <Typenav></Typenav>
+    <ListContainer></ListContainer>
+    <TodayRecommend></TodayRecommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Brand></Brand>
   </div>
 </template>
 
 <script>
+import Typenav from "./Typenav";
+import ListContainer from "./ListContainer";
+import TodayRecommend from "./TodayRecommend";
+import Rank from "./Rank";
+import Like from "./Like";
+import Floor from "./Floor";
+import Brand from "./Brand";
+
 export default {
-    name:'Home'
+    name:'Home',
+    components:{
+      Typenav,
+      ListContainer,
+      TodayRecommend,
+      Rank,
+      Like,
+      Floor,
+      Brand
+    }
 }
 </script>
 
